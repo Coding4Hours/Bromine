cd89dbdd2997a2d63690c03cf65d6b44f99b7fe3
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-19 17:43:11 -0700

    asdfasfdadsf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,51 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
-import icons from "astro-icons";
-
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
-
 import playformCompress from "@playform/compress";
+
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { bareModulePath } from "@mercuryworkshop/bare-as-module3";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icons(), playformCompress()],
-    vite: { plugins: [tailwindcss()] },
-    adapter: netlify(),
+	integrations: [icon(), playformCompress()],
+	vite: {
+		plugins: [
+			tailwindcss(),
+			viteStaticCopy({
+				targets: [
+					{
+						src: uvPath,
+						dest: "",
+						rename: "uv",
+						overwrite: false,
+					},
+					{
+						src: epoxyPath,
+						dest: "",
+						rename: "epoxy",
+						overwrite: false,
+					},
+					{
+						src: bareModulePath,
+						dest: "",
+						rename: "baremod",
+						overwrite: false,
+					},
+					{
+						src: baremuxPath,
+						dest: "",
+						rename: "baremux",
+					},
+				],
+			}),
+		],
+	},
+	output: "server",
+	adapter: netlify(),
 });
---
+++ public/sw.js
@@ -0,0 +1,24 @@
+if (navigator.userAgent.includes("Firefox")) {
+	Object.defineProperty(globalThis, "crossOriginIsolated", {
+		value: true,
+		writable: false,
+	});
+}
+
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts("/uv/uv.sw.js");
+
+const uv = new UVServiceWorker();
+
+async function handleRequest(event) {
+	if (uv.route(event)) {
+		return await uv.fetch(event);
+	}
+
+	return await fetch(event.request);
+}
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event));
+});
