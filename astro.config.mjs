a0af868ec693a71c184ca671361597b898525bb9
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-20 19:58:31 -0700

    asdfasdfasdfasdfasdfasfd

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,41 +11,43 @@ import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { bareModulePath } from "@mercuryworkshop/bare-as-module3";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon(), playformCompress()],
-	vite: {
-		plugins: [
-			tailwindcss(),
-			viteStaticCopy({
-				targets: [
-					{
-						src: uvPath,
-						dest: "",
-						rename: "uv",
-						overwrite: false,
-					},
-					{
-						src: epoxyPath,
-						dest: "",
-						rename: "epoxy",
-						overwrite: false,
-					},
-					{
-						src: bareModulePath,
-						dest: "",
-						rename: "baremod",
-						overwrite: false,
-					},
-					{
-						src: baremuxPath,
-						dest: "",
-						rename: "baremux",
-					},
-				],
-			}),
-		],
-	},
-	output: "server",
-	adapter: netlify(),
+    integrations: [icon(), playformCompress(), preact()],
+    vite: {
+        plugins: [
+            tailwindcss(),
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: uvPath,
+                        dest: "",
+                        rename: "uv",
+                        overwrite: false,
+                    },
+                    {
+                        src: epoxyPath,
+                        dest: "",
+                        rename: "epoxy",
+                        overwrite: false,
+                    },
+                    {
+                        src: bareModulePath,
+                        dest: "",
+                        rename: "baremod",
+                        overwrite: false,
+                    },
+                    {
+                        src: baremuxPath,
+                        dest: "",
+                        rename: "baremux",
+                    },
+                ],
+            }),
+        ],
+    },
+    output: "server",
+    adapter: netlify(),
 });
